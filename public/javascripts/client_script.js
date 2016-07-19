e6f2a0a7057b58f0db6a6f9be779447fcc68ddc5
Nguyễn Khánh Duy <duynk@appota.com>
2016-07-19 14:28:19 +0700

    a

--- public/javascripts/client_script.js
+++ public/javascripts/client_script.js
@@ -7,8 +7,15 @@ function watch(details_url) {
 				if(val === ''){
 					alert('Hiện tại chưa có link, mời bạn quay lại sau');
 				} else {
-					window.location.replace(val);
+					var win = window.open(val, '_blank');
+  					win.focus();
 				}
 			});
 		});
 }
+
+function is_mobile(req) {
+    var ua = req.header('user-agent');
+    if (/mobile/i.test(ua)) return true;
+    else return false;
+};
