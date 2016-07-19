63fbc5e5cff6c9ca4ab3f36faa48264fa0a6e105
Nguyễn Khánh Duy <duynk@appota.com>
2016-07-19 10:54:42 +0700

    ass

--- public/javascripts/client_script.js
+++ public/javascripts/client_script.js
@@ -1,12 +1,11 @@
 var request = require('request');
 
 function watch(details_url) {
-	$.get(
+	$.getJSON(
 		"http://bu.1ly.co:6868/euro/api/get_live_url",
 		{details_url : details_url},
 		function(data) {
-			var content = JSON.stringify(data);
-			var url = content.live_url;
+			var url = data.live_url;
 			alert(url);
 		}
 	);
