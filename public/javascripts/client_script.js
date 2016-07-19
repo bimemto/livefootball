cb795e9256084d112510bf372280ba3ee967edb7
Nguyễn Khánh Duy <duynk@appota.com>
2016-07-19 10:58:34 +0700

    ass

--- public/javascripts/client_script.js
+++ public/javascripts/client_script.js
@@ -1,20 +1,10 @@
-var request = require('request');
-
 function watch(details_url) {
 	$.getJSON(
 		"http://bu.1ly.co:6868/euro/api/get_live_url",
 		{details_url : details_url},
 		function(data) {
-			$.each( data, function( key, val ) {
+			$.each(data, function( key, val ) {
 				alert(key + ":" val);
 			});
-		}
-		);
-	// request.get('http://bu.1ly.co:6868/euro/api/get_live_url?details_url=' + details_url, function(error, response, body){
-	// 	if(error){
-	// 		console.log(error);
-	// 	} else {
-	// 		alert(body);
-	// 	}
-});
+		});
 }
